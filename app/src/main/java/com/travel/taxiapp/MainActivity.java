3a73782166abcf5c57e13c8c9d30f05243f89198
Santhosh <sanmiphone@gmail.com>
2020-07-16 15:34:47 +0530

    first inital commit

--- app/src/main/java/com/travel/taxiapp/MainActivity.java
+++ app/src/main/java/com/travel/taxiapp/MainActivity.java
@@ -3,12 +3,20 @@ package com.travel.taxiapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    Button Btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Btn1=findViewById(R.id.button);
+
     }
 }
